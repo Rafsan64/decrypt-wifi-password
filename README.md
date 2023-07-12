13d24bedf473502d373e32faf1358de4c48cf50f
Rafsan Siyam <130149584+quixotic-dev64@users.noreply.github.com>
2023-07-12 13:50:25 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Decrypt Wifi Passwords
+A simple program to decrypt wifi password saved on your machine. <br>
+
+## Usage
+python decryptwifi.py<br>
---
+++ decryptwifi.py
@@ -0,0 +1,35 @@
+import subprocess
+import csv
+
+results = []
+profiles = []
+count=0
+
+#(1) Find all wifi profiles
+cmd_results = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8', errors ="backslashreplace")
+cmd_results = cmd_results.split("\r\n")
+profiles = [cmd_result.split(": ")[-1] for cmd_result in cmd_results if "All User Profile" in cmd_result]
+
+
+if __name__ == '__main__':
+    try:
+        with open('passwords.csv', mode='w', newline='') as passwords_file:
+            csv_writer = csv.writer(passwords_file, delimiter=',')
+            csv_writer.writerow(["index","wifi","password"])
+
+            #(2) loop through all profiles and extract wifi password
+            for index,profile in enumerate(profiles):
+		
+                cmd_results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode('utf-8', errors ="backslashreplace")
+                password=[cmd_result.split(":")[1][1:] for cmd_result in cmd_results.split("\r\n") if "Key Content" in cmd_result]
+                if len(password) == 1:
+
+                    #(3) If available, save password into csv
+                    print("Sequence: %d"%(count))
+                    print("Wifi: %s \nPassword: %s"%(profile,password[0]))
+                    print("*"*50)
+                    csv_writer.writerow([count,profile,password[0]])
+                    count=count+1
+            
+    except Exception as e:
+        print("[ERR] "%str(e))
